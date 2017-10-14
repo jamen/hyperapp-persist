8c679c32e5dfa3260e25576943d45041986cefc9
Jamen Marz <jamenmarz+gh@gmail.com>
2017-10-13 19:43:05 -0600

    Remove arrow functions

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 
-module.exports = (app, opts) => props => {
-  opts = opts || {}
-  var storage = opts.storage || 'hyperapp-persist'
-  var include = opts.include || []
-  var _init = props.init
-  
-  props.init = function (state, actions) {
-    if (_init) _init.apply(null, arguments)
-
-    var data = localStorage.getItem(storage)
-    if (data) actions.__restore(JSON.parse(data))
+module.exports = function (app, opts) {
+  return function (props) {
+    opts = opts || {}
+    var storage = opts.storage || 'hyperapp-persist'
+    var include = opts.include || []
+    var _init = props.init
     
-    window.addEventListener('unload', function () {
-      actions.__save()
-    })
-  }
+    props.init = function (state, actions) {
+      if (_init) _init.apply(null, arguments)
 
-  props.actions.__restore = function (_s, _a, previous) {
-    return previous
+      var data = localStorage.getItem(storage)
+      if (data) actions.__restore(JSON.parse(data))
+      
+      window.addEventListener('unload', function () {
+	actions.__save()
+      })
+    }
+
+    props.actions.__restore = function (_s, _a, previous) {
+      return previous
+    }
+    
+    props.actions.__save = function (state) {
+      localStorage.setItem(storage, JSON.stringify(state, include))
+    }
+    
+    return app(props)
   }
-  
-  props.actions.__save = function (state) {
-    localStorage.setItem(storage, JSON.stringify(state, include))
-  }
-  
-  return app(props)
 } 
