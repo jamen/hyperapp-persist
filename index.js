c16dbdf55922c04c905c538b67e01691901cd871
Jamen Marz <jamenmarz+gh@gmail.com>
2017-11-10 07:01:02 -0700

    Start on IndexedDB implementation

--- index.js
+++ index.js
@@ -2,29 +2,43 @@
 module.exports = function (app, opts) {
   return function (props) {
     opts = opts || {}
+    var version = opts.version
     var storage = opts.storage || 'hyperapp-persist'
     var include = opts.include || []
     var _init = props.init
-    
+
+    var dbPromise = indexedDB.open(storage, version, function (dbUpgrade) {
+      if (!dbUpgrade.objectStoreNames.contains('state')) {
+        dbUpgrade.createObjectStore('state', { autoIncrement: true })
+      }
+    })
+
     props.init = function (state, actions) {
       if (_init) _init.apply(null, arguments)
 
-      var data = localStorage.getItem(storage)
-      if (data) actions.__restore(JSON.parse(data))
-      
-      window.addEventListener('unload', function () {
-	actions.__save()
+      dbPromise.then(function (db) {
+        var tx = db.transaction('state', 'readonly')
+        var store = tx.objectStore('state')
+
+        return store.getAll()
+      }).then(function (items) {
+        var data = items[0]
+        if (data) actions.__restore(data)
+
+        window.addEventListener('unload', function () {
+          actions.__save()
+        })
       })
     }
 
     props.actions.__restore = function (_s, _a, previous) {
       return previous
     }
-    
+
     props.actions.__save = function (state) {
       localStorage.setItem(storage, JSON.stringify(state, include))
     }
-    
+
     return app(props)
   }
-} 
+}
